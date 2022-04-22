14bb40cd225d1e963d39789e4d18c3701718e2ee
HokPuiLam <hlam6@sheffield.ac.uk>
2022-04-22 07:58:18 +0100

    task1 finished

--- src/task1.py
+++ src/task1.py
@@ -64,28 +64,62 @@ class Circle:
         self.ctrl_c = True
 
     def print_odom_readings(self):
-        print(f"x={self.x-self.x0:.2f} m, y={self.y-self.y0:.2f} yaw={self.theta_z-self.theta_z0:.1f} degrees")
+        print(f"x={self.x-self.x0:.4f} m, y={self.y-self.y0:.4f} yaw={self.theta_z-self.theta_z0:.4f} degrees")
 
-    def zero_positionify(zero_x, zero_y, zero_z, curr_x, curr_y, curr_z):
-        x = curr_x - zero_x
-        y = curr_y - zero_y
-        z = curr_z - zero_z
     def main_loop(self):
         status = ""
+        round = 0
         while not self.ctrl_c:
             # specify the radius of the circle:
-            path_rad = 0.5 # m
-            # linear velocity must be below 0.26m/s:
-            lin_vel = 0.2 # m/s
+            # if(self.theta_z-self.theta_z0 >= 0.00 and self.theta_z-self.theta_z0 < 0.1):
+            #     round = 0
+            #     print("round 0")
+            if(round == 0):
+                print("round 0")
 
-            self.vel_cmd.linear.x = lin_vel
-            self.vel_cmd.angular.z = lin_vel / path_rad # rad/s
+                path_rad = 0.5 # m
+                # linear velocity must be below 0.26m/s:
+                lin_vel = 0.2 # m/s
+                
 
-            #self.vel_cmd.linear.x = 0.0
-            #self.vel_cmd.angular.z = 0.0
-            self.print_odom_readings()
-            self.pub.publish(self.vel_cmd)
-            self.rate.sleep()
+                self.vel_cmd.linear.x = lin_vel
+                self.vel_cmd.angular.z = lin_vel / path_rad # rad/s
+
+                #self.vel_cmd.linear.x = 0.0
+                #self.vel_cmd.angular.z = 0.0
+                self.print_odom_readings()
+                self.pub.publish(self.vel_cmd)
+                self.rate.sleep()
+                if(self.theta_z-self.theta_z0 >= -0.1 and self.theta_z-self.theta_z0 < 0.0):
+                    round = 1
+                    print("test")
+            if(round == 1):
+                print("round 1")
+                path_rad = -0.5 # m
+                # linear velocity must be below 0.26m/s:
+                lin_vel = 0.2 # m/s
+                
+
+                self.vel_cmd.linear.x = lin_vel
+                self.vel_cmd.angular.z = lin_vel / path_rad # rad/s
+
+                #self.vel_cmd.linear.x = 0.0
+                #self.vel_cmd.angular.z = 0.0
+                self.print_odom_readings()
+                self.pub.publish(self.vel_cmd)
+                self.rate.sleep()
+                if(self.theta_z-self.theta_z0 >= 0.0 and self.theta_z-self.theta_z0 < 0.1):
+                    round = 2
+            if(round == 2):
+                self.vel_cmd.linear.x = 0.0 # m/s
+                self.vel_cmd.angular.z = 0.0 # rad/s
+
+                print("stopping the robot")
+
+                # publish to the /cmd_vel topic to make the robot stop
+                self.pub.publish(self.vel_cmd)
+                self.shutdownhook()
+                
 
 if __name__ == '__main__':
     vel_ctlr = Circle()
